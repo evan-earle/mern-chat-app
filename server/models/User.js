e6fc8fef1bbcff09cdbf353c1a60a1f98d6e81f3
Evan Earle <evan.earle@gmail.com>
2023-12-24 17:33:18 -0400

    added models

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
