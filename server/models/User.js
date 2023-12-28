17ed0df27ed3032a1c8e1a9418f2cd79ae8922e4
Evan Earle <evan.earle@gmail.com>
2023-12-28 14:51:00 -0400

    added auth routes, controllers, error handling

--- server/models/User.js
+++ server/models/User.js
@@ -10,6 +10,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
@@ -17,7 +18,7 @@ const userSchema = new Schema(
     },
     image: {
       type: String,
-      required: true,
+
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
